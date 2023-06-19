0c2b2e5d45a4e6a0458e0a59f234d31a06676b36
MatthewReineri <136749695+MatthewReineri@users.noreply.github.com>
2023-06-19 14:39:29 -0500

    Add files via upload

---
+++ quick_sort.py
@@ -0,0 +1,15 @@
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    stack = []
+    stack.append((0, len(arr) - 1))
+
+    while len(stack) > 0:
+        pass
+
+def main():
+    arr=(50, 30, 80 ,20, 10, 70, 60)
+    empty_arr = []
+    quicksort(empty_arr)
+
+main()
